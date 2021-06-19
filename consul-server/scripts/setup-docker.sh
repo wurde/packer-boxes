212e42c8207fba041a2dfc6409cda01d524c0fbc
Andy Bettisworth <andy.bettisworth@gmail.com>
2021-06-19 13:28:33 -0500

    setup alpine

--- consul-server/scripts/setup-docker.sh
+++ consul-server/scripts/setup-docker.sh
@@ -2,14 +2,16 @@
 
 function setTimezone() {
   echo "Setting timezone to UTC"
-  sudo timedatectl set-timezone UTC
+  apk add tzdata
+  cp /usr/share/zoneinfo/UTC /etc/localtime
+  echo "UTC" > /etc/timezone
 }
 
 # Alpine instances use the apk package manager. It can
 # install, remove, and update software.
 function updatePackages() {
   echo "Updating packages"
-  sudo apk -U upgrade
+  apk -U upgrade
 }
 
 function main() {
